09e80b564bc0a4e1e6cd6b150aeda46edb137f21
lisahtoledo <elisatoledo@hotmail.com.br>
2020-03-01 17:30:32 -0300

    Salva as sessões geradas em arquivos na pasta sessions/

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require("express"); 
 const uuid = require("uuid/v4");
 const session = require("express-session");
-
+const FileStore = require("session-file-store")(session); //TO DO: Substituir por um módulo que salva no BD.
 // cria o servidor 
 const app = express();  
 
@@ -13,6 +13,7 @@ app.use(session({
         console.log(req.sessionID);
         return uuid(); // usa UUID's para ID da sessão
     },
+    store: new FileStore(),
     secret: 'gato teclado', // na fase de produção isso deve ser alterado por uma string gerada aleatoriamente com base em uma environment variable.
     resave: false,
     saveUninitialized: true
