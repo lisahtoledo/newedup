e7fd797a75845e5ec7f31461d721e041b5eed720
lisahtoledo <elisatoledo@hotmail.com.br>
2020-03-01 17:45:35 -0300

    Inclui o body-parser para receber json.

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const express = require("express");
 const uuid = require("uuid/v4");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session); //TO DO: Substituir por um módulo que salva no BD.
+const bodyParser = require("body-parser"); 
+
 // cria o servidor 
 const app = express();  
 
 // adiciona e configura middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(session({
     genid: (req) => {
         console.log("Dentro do middleware de Sessão.")
@@ -20,11 +24,24 @@ app.use(session({
 }));
 
 // cria a rota para a homepage em '/'
-    app.get('/', (req, res) => {
-        console.log("Dentro da função callback da homepage.")
-        console.log(req.sessionID);
-        res.send(`Você chegou na nossa home!\n`); 
+app.get('/', (req, res) => {
+    console.log("Dentro da função callback da homepage.");
+    console.log(req.sessionID);
+    res.send(`Você chegou na nossa home!\n`); 
 }); 
+
+// cria as rotas de GET  e POST de login
+app.get('/login', (req, res) => {
+    console.log("Dentro da rota GET de login.");
+    console.log(req.sessionID);
+    res.send("Você chegou na página de login!\n");
+});
+
+app.post('/login', (req, res) => {
+    console.log("Dentro da rota POST de login.");
+    console.log(req.body); 
+    res.send("Você postou na página de login\n");
+});
 
 // diz para o servidor em que porta ouvir
 app.listen(3000, ()=>{
