311085259faa580e64835eb95a6065f94bd9c43a
lisahtoledo <elisatoledo@hotmail.com.br>
2020-03-05 19:44:03 -0300

    Dump do banco atualizado com todas as tabelas

--- edup_db_backup.sql
+++ edup_db_backup.sql
@@ -16,6 +16,94 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Alunos`
+--
+
+DROP TABLE IF EXISTS `Alunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Alunos` (
+  `cpf` varchar(14) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `fk_Usuario_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`cpf`,`fk_Usuario_id`),
+  KEY `fk_Usuario_id` (`fk_Usuario_id`),
+  CONSTRAINT `Alunos_ibfk_1` FOREIGN KEY (`fk_Usuario_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Alunos`
+--
+
+LOCK TABLES `Alunos` WRITE;
+/*!40000 ALTER TABLE `Alunos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Alunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Cursa`
+--
+
+DROP TABLE IF EXISTS `Cursa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cursa` (
+  `fk_Aluno_CPF` varchar(14) DEFAULT NULL,
+  `fk_Aluno_fk_Usuario_id` int(11) unsigned DEFAULT NULL,
+  `fk_Curso_id` int(11) DEFAULT NULL,
+  KEY `fk_Aluno_CPF` (`fk_Aluno_CPF`,`fk_Aluno_fk_Usuario_id`),
+  KEY `fk_Cursa_Cursos` (`fk_Curso_id`),
+  CONSTRAINT `Cursa_ibfk_1` FOREIGN KEY (`fk_Aluno_CPF`, `fk_Aluno_fk_Usuario_id`) REFERENCES `Alunos` (`cpf`, `fk_Usuario_id`),
+  CONSTRAINT `fk_Cursa_Cursos` FOREIGN KEY (`fk_Curso_id`) REFERENCES `Cursos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cursa`
+--
+
+LOCK TABLES `Cursa` WRITE;
+/*!40000 ALTER TABLE `Cursa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cursa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Cursos`
+--
+
+DROP TABLE IF EXISTS `Cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `vagas` int(5) NOT NULL,
+  `data_inicio` date NOT NULL,
+  `data_termino` date NOT NULL,
+  `carga_horaria` int(5) NOT NULL,
+  `prazo` datetime NOT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `fk_Empresa_cnpj` int(10) NOT NULL,
+  `fk_Empresa_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Empresa_cnpj` (`fk_Empresa_cnpj`,`fk_Empresa_id`),
+  CONSTRAINT `Cursos_ibfk_1` FOREIGN KEY (`fk_Empresa_cnpj`, `fk_Empresa_id`) REFERENCES `Empresas` (`cnpj`, `usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cursos`
+--
+
+LOCK TABLES `Cursos` WRITE;
+/*!40000 ALTER TABLE `Cursos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Empresas`
 --
 
@@ -78,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-05 18:55:58
+-- Dump completed on 2020-03-05 19:43:16
