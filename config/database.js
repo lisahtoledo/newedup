b0eaf70dbd624d210030496979f1f3e176cd61ad
lisahtoledo <elisatoledo@hotmail.com.br>
2020-03-15 20:01:19 -0300

    Adiciona BD do addon ClearBD do heroku em config/database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     'connection': {
-            'host': 'localhost',
-            'user': 'root',
-            'password': 'Theo2305!',
-            'database': "edup"
+            'host': 'us-cdbr-iron-east-04.cleardb.net',
+            'user': 'b63f60b4a07b82',
+            'password': '7846da2f',
+            'database': "heroku_4253d3545899495"
         },
     
     'users_table': "Users"
